7d736acccc262e49f3226dde445487068a2dc950
Sanjana Das <sanjana.das@vanderbilt.edu>
2024-10-03 21:00:04 -0500

    complete cloud vars gen

--- src/plugins/variableFileGen/variableFileGen/__init__.py
+++ src/plugins/variableFileGen/variableFileGen/__init__.py
@@ -41,18 +41,20 @@ class variableFileGen(PluginBase):
         
         self.vars_string=''
         self.ubuntu_info_generate()
-        self.vars_string+=self.ubuntu_info_string
+        self.k8s_info_generate()
+        self.cloud_info_generate()
+        self.docker_info_generate()
+        self.helms_info_generate()
+        self.ovs_info_generate()
+        self.kafka_info_generate()
 
-        
-        # self.generate_subnet_config()
-        # self.generate_hosts_config()
-        # self.generate_agent_config()
-        
-        # self.cyborg_config=''
-        # self.cyborg_config+= self.agent_string
-        # self.cyborg_config+=self.host_string
-        # self.cyborg_config+=self.subnet_string
-        
+        self.vars_string+=self.ubuntu_info_string
+        self.vars_string+=self.k8s_info_string
+        self.vars_string+=self.cloud_info_string
+        self.vars_string+=self.docker_info_string
+        self.vars_string+=self.helms_info_string
+        self.vars_string+=self.ovs_info_string
+        self.vars_string+=self.kafka_info_string
 
         # Return the config file
         output_filename = self.get_current_config().get("file_name")
@@ -80,10 +82,94 @@ class variableFileGen(PluginBase):
             # print("attributes of ubuntu", self.core.get_attribute_names(node))
             attribute_names=self.core.get_attribute_names(node)
             for name in attribute_names:
-                self.ubuntu_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
+                if name!='name':
+                    self.ubuntu_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
 
         
         print(self.ubuntu_info_string)
+
+    def k8s_info_generate(self):
+        self.k8s_info_string = ''
+        k8s_names,k8s_nodes=self.get_objs_of_meta('K8s')
+        self.k8s_info_string +="K8s:\n"
+        for node in k8s_nodes:
+            # print("attributes of k8s", self.core.get_attribute_names(node))
+            attribute_names=self.core.get_attribute_names(node)
+            for name in attribute_names:
+
+                if name=="teams":
+                    self.k8s_info_string+=f"    {name}:\n"
+                    # print("teams: ",self.core.get_attribute(node,name))
+                    team_info=self.core.get_attribute(node,name).split('\n')
+                    for team in team_info:
+                        self.k8s_info_string+=f"        - {team}\n"
+                    # self.k8s_info_string+=f"        - {self.core.get_attribute(node,'name')}\n"
+
+                elif name!='name':
+                    self.k8s_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
+    
+    def cloud_info_generate(self):
+        self.cloud_info_string = ''
+        cloud_names,cloud_nodes=self.get_objs_of_meta('cloud')
+        self.cloud_info_string +="cloud:\n"
+        for node in cloud_nodes:
+            # print("attributes of cloud", self.core.get_attribute_names(node))
+            attribute_names=self.core.get_attribute_names(node)
+            for name in attribute_names:
+
+                if name == "bastion_security_groups" or name =="vm_security_groups":
+                    print("name: ",name)
+                    self.cloud_info_string+=f"    {name}:\n"
+                    security_info=self.core.get_attribute(node,name).split('\n')
+                    for info in security_info:
+                        self.cloud_info_string+=f"        - {info}\n"
+
+                elif name!='name':
+                    self.cloud_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
+
+    def docker_info_generate(self):
+        self.docker_info_string = ''
+        docker_names,docker_nodes=self.get_objs_of_meta('Docker')
+        self.docker_info_string +="docker:\n"
+        for node in docker_nodes:
+            # print("attributes of docker", self.core.get_attribute_names(node))
+            attribute_names=self.core.get_attribute_names(node)
+            for name in attribute_names:
+                if name!='name':
+                    self.docker_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
+    
+    def helms_info_generate(self):
+        self.helms_info_string = ''
+        helms_names,helms_nodes=self.get_objs_of_meta('helm')
+        self.helms_info_string +="helm:\n"
+        for node in helms_nodes:
+            # print("attributes of helm", self.core.get_attribute_names(node))
+            attribute_names=self.core.get_attribute_names(node)
+            for name in attribute_names:
+                if name!='name':
+                    self.helms_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
+
+    def ovs_info_generate(self):
+        self.ovs_info_string = ''
+        ovs_names,ovs_nodes=self.get_objs_of_meta('ovs')
+        self.ovs_info_string +="ovs:\n"
+        for node in ovs_nodes:
+            # print("attributes of ovs", self.core.get_attribute_names(node))
+            attribute_names=self.core.get_attribute_names(node)
+            for name in attribute_names:
+                if name!='name':
+                    self.ovs_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
+    
+    def kafka_info_generate(self):
+        self.kafka_info_string = ''
+        kafka_names,kafka_nodes=self.get_objs_of_meta('kafka')
+        self.kafka_info_string +="kafka:\n"
+        for node in kafka_nodes:
+            # print("attributes of kafka", self.core.get_attribute_names(node))
+            attribute_names=self.core.get_attribute_names(node)
+            for name in attribute_names:
+                if name!='name':
+                    self.kafka_info_string+=f"    {name}: {self.core.get_attribute(node,name)}\n"
 
 
 
